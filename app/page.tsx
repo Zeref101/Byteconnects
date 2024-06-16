12294e5e18779eeff5277cdd3adb54429a2720fc
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-06-16 11:11:52 +0530

    chore: Fix typo in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-'use client';
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#2b3853]">
-      <Button onClick={() => {
-        alert("Woooohooooo")
-      }}>
+      <Button
+        onClick={() => {
+          alert("Woooohooooo");
+        }}
+      >
         Click here
       </Button>
     </main>
