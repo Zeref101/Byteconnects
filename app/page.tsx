1afc1e1365d7ceed7a972100bd48ac15bb8c6b59
Zeref101 <shreyas.mohanty03@gmail.com>
2024-06-19 19:30:01 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Blogcard from "@/components/Blog-Card/Blogcard";
 import Navbar from "@/components/Navbar/Navbar";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#181A2A]">
       <Navbar />
+      <Blogcard />
       <Button onClick={() => {
         alert("Woooohooooo")
       }}>
