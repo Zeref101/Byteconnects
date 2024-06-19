f079185eb1cb4015ad85b8dea1aa61ee9a51e53c
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-06-20 02:23:14 +0530

    feat: Add verification for existing users in emailSignUp function
    The commit modifies the emailSignUp function in the AuthContext to include verification for existing users. If a user tries to sign up with an email that already exists in the database, an error message is displayed to the user. This change improves the user experience and prevents duplicate user entries in the database.

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -12,19 +12,22 @@ import {
 import { getDoc, doc, setDoc } from "firebase/firestore";
 import { createContext, useContext, useEffect, useState } from "react";
 import { app, auth, db } from "@/firebase";
+import { useToast } from "@/components/ui/use-toast";
 const AuthContext = createContext({});
 
-async function verifyUser(user: User) {
-  const docRef = doc(db, `users/${user.uid}`);
+async function verifyUser(user: User, password?: string, username?: string) {
+  const docRef = doc(db, `users/${user.email}`);
   const docSnap = await getDoc(docRef);
   if (!docSnap.exists()) {
     await setDoc(docRef, {
       email: user.email,
-      password: "",
+      password: password ?? "",
       photoURL: user.photoURL,
-      username: user.displayName,
+      username: user.displayName ?? username,
       uid: user.uid,
     });
+  } else {
+    throw new Error("User already exists");
   }
 }
 
@@ -35,11 +38,12 @@ export const AuthContextProvider = ({
 }) => {
   const [user, setUser] = useState<User | null>();
   const [loading, setLoading] = useState<boolean>(false);
+  const { toast } = useToast();
 
   const signInWithGoogle = async () => {
+    setLoading(true);
     const provider = new GoogleAuthProvider();
     try {
-      setLoading(true);
       const { user } = await signInWithPopup(auth, provider);
       setUser(user);
       verifyUser(user);
@@ -55,7 +59,11 @@ export const AuthContextProvider = ({
     setLoading(false);
   };
 
-  const emailSignUp = async (email: string, password: string) => {
+  const emailSignUp = async (
+    username: string,
+    email: string,
+    password: string
+  ) => {
     try {
       setLoading(true);
       const { user } = await createUserWithEmailAndPassword(
@@ -65,9 +73,15 @@ export const AuthContextProvider = ({
       );
       setUser(user);
       console.log(user);
-      verifyUser(user);
-    } catch (error) {
-      console.log(error);
+      await verifyUser(user, password, username);
+      toast({
+        title: "Account created successfully!",
+      });
+    } catch (error: any) {
+      toast({
+        variant: "destructive",
+        title: error.message,
+      });
     } finally {
       setLoading(false);
     }
