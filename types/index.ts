df95a65ae7550656c9228e9d4100610f52a6ef9c
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-06-20 02:28:03 +0530

    feat: Add AuthContextType interface for authentication functionality

--- types/index.ts
+++ types/index.ts
@@ -5,4 +5,19 @@ export interface CustomAuthProps {
   image: StaticImageData;
   redirectUrl: string;
   signingMethod: string;
+  handleClick: () => void;
+}
+
+export interface AuthContextType {
+  emailSignUp?: (
+    username: string,
+    email: string,
+    password: string
+  ) => Promise<void>;
+  signInWithGoogle: () => Promise<void>;
+  loading: boolean;
+  emailSignIn?: (
+    email: string,
+    password: string
+  ) => Promise<void>;
 }
