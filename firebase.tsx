a17dacb13ef0f3b9013e46596fb8e1f7ae336d69
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-07-02 19:52:27 +0530

    ```text feat: Add Firebase storage functionality
    The commit adds Firebase storage functionality by importing the `getStorage` function from the `firebase/storage` module. This allows the application to interact with Firebase storage for uploading and retrieving files.

--- firebase.tsx
+++ firebase.tsx
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,7 +13,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDg3uF4MZwaz_0pNrQZIRKcA1KVzAdlGyI",
   authDomain: "byteconnects.firebaseapp.com",
   projectId: "byteconnects",
-  storageBucket: "byteconnects.appspot.com",
+  storageBucket: "gs://byteconnects.appspot.com",
   messagingSenderId: "88863885057",
   appId: "1:88863885057:web:49ade0fca82e1c1ab97d00",
   measurementId: "G-HK034B2E6N",
@@ -23,3 +24,4 @@ export const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
