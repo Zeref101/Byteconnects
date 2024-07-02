1d8744b6ca3ae23c004ee3a89800b20515dff4cb
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-07-03 00:23:39 +0530

    feat: Update HorizontalLine component to use black background color
    This commit updates the HorizontalLine component to use a black background color instead of white. This change improves the visual appearance of the component and ensures consistency with the design of the application.

--- components/horizontal-line/HorizontalLine.tsx
+++ components/horizontal-line/HorizontalLine.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HorizontalLine = () => {
     return (
         <div className="flex w-full items-center">
-            <div className="h-0.5 w-full bg-white" />
+            <div className="h-0.5 w-full bg-black" />
             <div className="h-5 w-20 bg-white" />
             <div className="h-0.5 w-full bg-white" />
         </div>
