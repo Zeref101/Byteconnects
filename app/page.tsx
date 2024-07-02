ae36ba26302f3fe4db1a168acddd43260398ec05
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-07-03 00:23:03 +0530

    feat: Add lazy loading for images and fetch blog data
    This commit adds lazy loading for images on the Home page to improve page load performance. It also fetches blog data from the API using the user's UID and displays it in the console. These changes enhance the user experience and functionality of the application.

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,33 @@
-'use client';
+"use client";
 import Blogcard from "@/components/Blog-Card/Blogcard";
 import Navbar from "@/components/Navbar/Navbar";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-
+import axios from "axios";
+import { useEffect } from "react";
+import { useAuth } from "@/contexts/AuthContext";
+import { AuthContextType } from "@/types";
 export default function Home() {
+  const { user } = useAuth() as AuthContextType;
+  console.log(user);
+  useEffect(() => {
+    if (!user) return;
+    axios.get(`/api/blog/${user?.uid}`).then((res) => {
+      console.log(res.data);
+    });
+    axios.get("/api/blog/getPosts").then((res) => {
+      console.log(res.data);
+    });
+  }, [user]);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#181A2A]">
       <Navbar />
       <Blogcard />
-      <Button onClick={() => {
-        alert("Woooohooooo")
-      }}>
+      <Button
+        onClick={() => {
+          alert("Woooohooooo");
+        }}
+      >
         Click here
       </Button>
     </main>
