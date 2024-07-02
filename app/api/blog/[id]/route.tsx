40e5830b2bfafcc632907e79519d365b9b0fc60e
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-07-03 00:23:09 +0530

    feat: Add API route for fetching blog data by ID
    This commit adds a new API route for fetching blog data by ID. It uses Firebase Firestore to query the "blogs" collection and retrieve the documents that match the provided ID. The fetched data is then logged to the console. This change enhances the functionality of the application by allowing users to retrieve specific blog data based on the ID parameter.

---
+++ app/api/blog/[id]/route.tsx
@@ -0,0 +1,19 @@
+import { db } from "@/firebase";
+import { getDocs, collection, query, where, limit } from "firebase/firestore";
+
+export const GET = async (req: any, { params }: any) => {
+  console.log(params.id);
+  const q = query(
+    collection(db, "blogs"),
+    where("uid", "==", params.id),
+    limit(6)
+  );
+  const docs: any = [];
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+    docs.push(doc.data());
+  });
+  return new Response(JSON.stringify(docs), { status: 200 });
+};
