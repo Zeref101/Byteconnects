755d2aa2b4b7157ce2313184684518c75daab168
Zeref101 <shreyas.mohanty03@gmail.com>
2024-07-07 13:16:57 +0530

    feat: Update RTE component with initial content and content update handling
    This commit updates the RTE (Rich Text Editor) component to include initial content and handle content updates. It sets the initial content of the editor to the value provided in the `initialContent` prop. Additionally, it adds an effect to update the editor content when the `initialContent` prop changes. This ensures that the editor always displays the correct content. The commit also includes a separate effect to update the `setText` state when the editor content changes, allowing the parent component to access and use the updated content. These changes improve the functionality and usability of the RTE component.

--- components/RTE/RTE.tsx
+++ components/RTE/RTE.tsx
@@ -1,36 +1,58 @@
-import "./styles.scss";
+import "./style.css";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useEffect } from "react";
 import { Button } from "../ui/button";
 import { MenuBar } from "./MenuBar";
+
 export default function Tiptap({
   onClick,
   setText,
+  initialContent
 }: {
   onClick: () => void;
   setText: Dispatch<SetStateAction<string | undefined>>;
+  initialContent: string
 }) {
   const editor = useEditor({
     extensions: [StarterKit],
-    content: "Hello World",
+    content: initialContent, // Use initialContent as the initial content
     editorProps: {
       attributes: {
-        // spellcheck: "false",
-        class:
-          "prose prose-sm p-4 sm:prose border-2 border-black lg:prose-lg xl:prose-2xl mx-auto focus:outline-none",
+        class: "prose prose-sm p-4 sm:prose border-2 border-black lg:prose-lg xl:prose-2xl mx-auto focus:outline-none",
       },
     },
   });
+
+  // Effect to update editor content when initialContent changes
+  useEffect(() => {
+    if (editor && initialContent !== editor.getHTML()) {
+      editor.commands.setContent(initialContent);
+    }
+  }, [initialContent, editor]);
+
+  useEffect(() => {
+    if (editor) {
+      const updateHandler = () => {
+        const html = editor.getHTML();
+        setText(html);
+      };
+      editor.on('update', updateHandler);
+      return () => {
+        editor?.off('update', updateHandler);
+      };
+    }
+  }, [editor, setText]);
+
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col items-center justify-center gap-4">
       <MenuBar editor={editor} />
       <EditorContent editor={editor} />
       <Button
         className="max-w-[100px] self-center"
         onClick={() => {
           setText(editor?.getHTML());
-          // onClick();
+          // onClick(); // Uncomment if onClick should be called
           console.log(editor?.getHTML());
         }}
       >
